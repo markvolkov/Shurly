c0efbec4c51f59bb8f1ff2cda9fcb434dbab4172
BuildTools <markvolkov@me.com>
2020-10-18 23:36:48 -0400

    initial commit with completed project

---
+++ README.md
@@ -0,0 +1,5 @@
+---
+# Systems Engineering Project
+> Submission for CloudFlare-Hiring 2020
+> By Mark Volkov
+---
---
+++ main.go
@@ -0,0 +1,254 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"net"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func minInt(x, y uint64) uint64 {
+	if x > y {
+		return y
+	} else {
+		return x
+	}
+}
+
+func maxInt(x, y uint64) uint64 {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func minByte(x, y byte) byte {
+	if x > y {
+		return y
+	} else {
+		return x
+	}
+}
+
+func maxByte(x, y byte) byte {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func sumBytes(bytes []byte) byte {
+	result := byte(0)
+	for _, v := range bytes {
+		result += v
+	}
+	return result
+}
+
+func readStatusCode(body string) uint16 {
+	leftOfStatus := strings.Index(body, " ")
+	statusCodeAsString := body[leftOfStatus + 1 : (leftOfStatus + 1 + 4)]
+	res, _ := strconv.ParseUint(strings.TrimSpace(statusCodeAsString), 10, 64)
+	statusCodeResult := uint16(res)
+	return statusCodeResult
+}
+
+const requestDelay = 2 //seconds
+
+type Response struct {
+	TimeInMs     uint64 `json:"timeInMs"`
+	Body         string `json:"body"`
+	StatusCode   uint16 `json:"statusCode"`
+	ResponseSize byte   `json:"responseSize(bytes)"`
+}
+
+type Request struct {
+	Address string `json:"address"`
+	AddressStripped string `json:"addressStripped"`
+	Route   string `json:"requestedRoute"`
+}
+
+type Profiler struct {
+	RequestCount           *uint64 `json:"totalRequestCount"`
+	FailedRequestCount     *uint64 `json:"failedRequestCount"`
+	SuccessfulRequestCount *uint64 `json:"successfulRequestCount"`
+	ErrorCodes []uint16 `json:"errorCodes,omitempty"`
+
+	Request Request `json:"originalRequest"`
+
+	MinResponseSize byte `json:"minResponseSize"`
+	MaxResponseSize byte `json:"maxResponseSize"`
+
+	MeanResponseTime    uint64 `json:"meanResponseTime"`
+	MedianResponseTime  uint64 `json:"medianResponseTime"`
+	FastestResponseTime uint64 `json:"fastestResponseTime"`
+	SlowestResponseTime uint64 `json:"slowestResponseTime"`
+}
+
+func (profiler *Profiler) String() string {
+	return fmt.Sprintf("Percentage of successful requests (%d / %d) = %%%d", *profiler.SuccessfulRequestCount, *profiler.RequestCount, (*profiler.SuccessfulRequestCount / *profiler.RequestCount)*100)
+}
+
+func (res *Response) Print() {
+	json.NewEncoder(os.Stdout).Encode(res)
+}
+
+func (profiler *Profiler) Print() {
+	json.NewEncoder(os.Stdout).Encode(profiler)
+}
+
+func (req *Request) GetResponse() (Response, error) {
+	start := time.Now()
+	conn, err := net.Dial("tcp", req.AddressStripped + ":443")
+	if err != nil {
+		return Response{}, err
+	}
+	cf := &tls.Config{Rand: rand.Reader, InsecureSkipVerify: true}
+	ssl := tls.Client(conn, cf)
+	if err != nil {
+		panic(err)
+	}
+	defer ssl.Close()
+	tcpRequestString := "GET " + req.Route + " HTTP/1.1\r\nHost: " + req.Address + "\r\nConnection: close\r\nContent-Type: text/html charset=utf-8;\r\nAccept: */*\r\n\r\n"
+	_, err = ssl.Write([]byte(tcpRequestString))
+	if err != nil {
+		return Response{}, err
+	}
+	result, err := ioutil.ReadAll(ssl)
+	if err != nil {
+		return Response{}, err
+	}
+	end := time.Now()
+	responseResult := Response{}
+	elapsed := (end.Unix() * 1000) - (start.Unix() * 1000)
+	if elapsed >= 0 {
+		responseResult.TimeInMs = uint64(elapsed)
+	} else {
+		responseResult.TimeInMs = 0
+	}
+	responseResult.Body = string(result)
+	responseResult.ResponseSize = sumBytes(result)
+	responseResult.StatusCode = readStatusCode(responseResult.Body)
+	return responseResult, nil
+}
+
+func (profiler *Profiler) StartProfiling() {
+	responseTimes := make([]uint64, 0)
+	var responseTimeRunningSum uint64 = 0
+	reqCount := *profiler.RequestCount
+	for reqCount > 0 {
+		currentResponse, err := profiler.Request.GetResponse()
+		if err != nil {
+			//request failed
+			*profiler.FailedRequestCount++
+		} else if currentResponse.StatusCode != 200 {
+			profiler.ErrorCodes = append(profiler.ErrorCodes, currentResponse.StatusCode)
+			*profiler.FailedRequestCount++
+		} else {
+			profiler.MinResponseSize = minByte(profiler.MinResponseSize, currentResponse.ResponseSize)
+			profiler.MaxResponseSize = maxByte(profiler.MaxResponseSize, currentResponse.ResponseSize)
+			profiler.SlowestResponseTime = maxInt(profiler.SlowestResponseTime, currentResponse.TimeInMs)
+			profiler.FastestResponseTime = minInt(profiler.FastestResponseTime, currentResponse.TimeInMs)
+
+			responseTimes = append(responseTimes, currentResponse.TimeInMs)
+			responseTimeRunningSum += currentResponse.TimeInMs
+
+			*profiler.SuccessfulRequestCount++
+			time.Sleep(requestDelay * time.Second)
+		}
+		reqCount--
+	}
+
+	sort.Slice(responseTimes, func(i, j int) bool {
+		return i < j
+	})
+
+	if len(responseTimes) > 0 {
+		isEven := len(responseTimes) % 2 == 0
+		if isEven {
+			leftIdx := len(responseTimes) / 2
+			rightIdx := (len(responseTimes) / 2) + 1
+			profiler.MedianResponseTime = (responseTimes[leftIdx] + responseTimes[rightIdx]) / 2
+		} else {
+			medianIdx := len(responseTimes) / 2
+			profiler.MedianResponseTime = responseTimes[medianIdx]
+		}
+	} else {
+		profiler.MedianResponseTime = math.MaxUint64
+	}
+	if *profiler.SuccessfulRequestCount == 0 {
+		profiler.MeanResponseTime = math.MaxUint64
+	} else {
+		profiler.MeanResponseTime = responseTimeRunningSum / uint64(*profiler.SuccessfulRequestCount)
+	}
+}
+
+func main() {
+	url := flag.String("url", "", "The url to request")
+	profile := flag.Uint64("profile", 0, "The amount of requests to profile")
+	flag.Parse()
+	if *profile < 0 {
+		log.Println("Your profile count parameter must be positive!")
+		os.Exit(1)
+	} else if len(*url) == 0 {
+		log.Fatal("You must provide a -url argument. Example -url=\"cloudflare.com\"")
+	} else {
+		address, route, addressStripped := parseAddressAndRoute(*url)
+		request := &Request{Address: address, AddressStripped: addressStripped, Route: route}
+		if *profile == 0 {
+			response, err := request.GetResponse()
+			if err != nil {
+				log.Println("There was an error handling your request")
+				log.Fatal(err.Error())
+				os.Exit(1)
+			}
+			response.Print()
+		} else {
+			profiler := &Profiler{ RequestCount: profile, FailedRequestCount: new(uint64), SuccessfulRequestCount: new(uint64),
+				MinResponseSize: 0, MaxResponseSize: 0, FastestResponseTime: math.MaxUint64, SlowestResponseTime: 0, Request: *request, ErrorCodes: make([]uint16, 0) }
+			profiler.StartProfiling()
+			profiler.Print()
+			//percentage stat
+			log.Println(profiler)
+		}
+	}
+	os.Exit(0)
+}
+
+
+func parseAddressAndRoute(url string) (string, string, string) {
+	address := url[:strings.LastIndex(url, ".")]
+	route := ""
+	for idx := strings.LastIndex(url, "."); idx < len(url); idx++ {
+		if url[idx:idx + 1] != "/" {
+			address += url[idx:idx + 1]
+		} else {
+			route = url[idx:]
+			break
+		}
+	}
+	addressStripped := address
+	if strings.HasPrefix(address, "http") || strings.HasPrefix(address, "www.") {
+		addressStripped = address[strings.Index(address, ".") + 1:]
+	}
+	routeIdx := strings.Index(addressStripped, "/")
+	if routeIdx != -1 {
+		route = addressStripped[routeIdx:]
+	}
+	if route == "" {
+		route = "/"
+	}
+	return address, route, addressStripped
+}
